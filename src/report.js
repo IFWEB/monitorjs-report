68127a299e3606598edbe756f3d09bca374be62d
chua1989 <chen_hua89@163.com>
2017-06-22 16:00:13 +0800

    添加report.js

---
+++ src/report.js
@@ -0,0 +1,13 @@
+/**
+<script>
+var _monitorjs = {};
+(function() {
+  var mo = document.createElement("script");
+  mo.src = "http://www.monitorjs.com/report.js?a041a0f4ff93aef6aa83f34134331a1d";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(mo, s);
+})();
+</script>
+*/
+
+
